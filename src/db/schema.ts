754d342468245fcad07aaeab7b35f03a44eac509
Davi Brito <daviibrito111@gmail.com>
2024-09-13 01:44:11 -0300

    feat: change table ids to cuid2

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,10 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
 
 export const goal = pgTable('goal', {
-    id: text('id').primaryKey(),
+    id: text('id')
+        .primaryKey()
+        .$defaultFn(() => createId()),
     title: text('title').notNull(),
     weeklyFrequency: integer('weekly_frequency').notNull(),
     createdAt: timestamp('created_at', { withTimezone: true })
@@ -10,7 +13,9 @@ export const goal = pgTable('goal', {
 })
 
 export const goalCompletion = pgTable('goal_completion', {
-    id: text('id').primaryKey(),
+    id: text('id')
+        .primaryKey()
+        .$defaultFn(() => createId()),
     goalId: text('goal_id')
         .notNull()
         .references(() => goal.id),
